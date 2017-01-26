73153e2055d23d514da50a6f9418b9d13f764e20
Oleg Lindvin <oleg.lindvin@gmail.com>
2017-01-26 16:51:43 +0100

    ReactNative Albums done

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { ScrollView } from 'react-native';
+
+import AlbumDetail from './AlbumDetail';
 
 class AlbumList extends Component {
 
@@ -13,12 +15,18 @@ class AlbumList extends Component {
 			});
 	}
 
+	renderAlbums() {
+		return this.state.albums.map(album => 
+			<AlbumDetail key={album.title} album={album} />
+		);
+	}
+
 	render() {
 		console.log(this.state);
 		return (
-			<View>
-				<Text>Album List!!!</Text>
-			</View>
+			<ScrollView>
+				{this.renderAlbums()}
+			</ScrollView>
 		);
 	}
 }
